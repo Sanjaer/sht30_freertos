c4a82e4c2e4655b84678fb8d90aedc638e72ad80
sanjo <pabloguinness@gmail.com>
2020-11-01 21:41:14 +0100

    Added configuration of I2C pins from menuconfig

--- main/sht30.h
+++ main/sht30.h
@@ -37,8 +37,8 @@
 #include "driver/i2c.h"
 
 
-#define I2C_EXAMPLE_MASTER_SCL_IO           0                /*!< gpio number for I2C master clock */
-#define I2C_EXAMPLE_MASTER_SDA_IO           4                /*!< gpio number for I2C master data  */
+#define I2C_EXAMPLE_MASTER_SCL_IO           CONFIG_SCL_PIN   /*!< gpio number for I2C master clock */
+#define I2C_EXAMPLE_MASTER_SDA_IO           CONFIG_SDA_PIN   /*!< gpio number for I2C master data  */
 #define I2C_EXAMPLE_MASTER_NUM              I2C_NUM_0        /*!< I2C port number for master dev */
 #define I2C_EXAMPLE_MASTER_TX_BUF_DISABLE   0                /*!< I2C master do not need buffer */
 #define I2C_EXAMPLE_MASTER_RX_BUF_DISABLE   0                /*!< I2C master do not need buffer */
